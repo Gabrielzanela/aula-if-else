703caf13f54c0913f08013994c5a5461bc78da12
Gabriel zanela machado de souza <101649513+Gabrielzanela@users.noreply.github.com>
2022-03-22 12:35:40 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+x = Math.floor(math.randoom()  * 100)
+console.log(x)
+
+if (x >= 7){
+    console.log("aluno aprovado")
+} else if(x >= 3){
+    console.log("aluno recuperação")
+} else {
+    console.log("aluno reprovado")
+}
+       
